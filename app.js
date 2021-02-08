c7626c31d09e847f2e15c9fd1876f492e5bf76d1
Shafin The Developer <sahariar350@gmail.com>
2021-02-09 01:10:37 +0600

    Data cradit added

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf')
     .then(res => res.json())
     .then(data => {
+        console.log(data);
         const charecters = document.getElementById('charecters');
-
         data.data.results.forEach(element => {
-            console.log(element);
             const thumbnailPath = element.thumbnail.path.split('ttp')[0] + 'ttps' + element.thumbnail.path.split('ttp')[1];
-            console.log(thumbnailPath);
             const card = document.createElement('div');
             card.innerHTML = `
             <img src="${ thumbnailPath }.${ element.thumbnail.extension }">
@@ -15,4 +13,6 @@ fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda28
             card.className = "card";
             charecters.appendChild(card);
         });
+        const dataProvider = document.getElementById('dataProvider');
+        dataProvider.innerHTML = data.attributionHTML;
     })
