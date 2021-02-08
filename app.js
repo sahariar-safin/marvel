d8087767e654cfaa3eddc019d86cf68bf774ae67
Shafin The Developer <sahariar350@gmail.com>
2021-02-09 00:57:15 +0600

    few issues fixed

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf')
+fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf')
     .then(res => res.json())
     .then(data => {
         console.log(data.data.results);
         const charecters = document.getElementById('charecters');
+        const thumbnailPath = element.thumbnail.path.split('ttp')[0] + 'ttps' + element.thumbnail.path.split('ttp')[1];
         data.data.results.forEach(element => {
             console.log(element);
             const card = document.createElement('div');
             card.innerHTML = `
-            <img src="${ element.thumbnail.path }.${ element.thumbnail.extension }">
+            <img src="${ thumbnailPath }.${ element.thumbnail.extension }">
             <h1>${ element.name }</h1>
         `;
             card.className = "card";
