5920db9c53c608b2639165924040832642d0998e
Shafin The Developer <sahariar350@gmail.com>
2021-02-09 13:33:18 +0600

    Search option added

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf')
+fetch('https://gateway.marvel.com/v1/public/characters?limit=50&ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf')
     .then(res => res.json())
     .then(data => {
-        console.log(data);
         const charecters = document.getElementById('charecters');
         data.data.results.forEach(element => {
             const thumbnailPath = element.thumbnail.path.split('ttp')[0] + 'ttps' + element.thumbnail.path.split('ttp')[1];
@@ -16,3 +15,24 @@ fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=08be70c1cda28
         const dataProvider = document.getElementById('dataProvider');
         dataProvider.innerHTML = data.attributionHTML;
     })
+
+    
+const handleSearch = () => {
+    const searchInput = document.getElementById('searchInput').value;
+    fetch(`https://gateway.marvel.com/v1/public/characters?name=${ searchInput }&ts=1&apikey=08be70c1cda287ae92195e56b0641b0f&hash=cdf63316afdae7ebd245284e7060cecf`)
+        .then(res => res.json())
+        .then(data => {
+            const charecters = document.getElementById('charecters');
+            document.getElementById('charecters').innerHTML = '';
+            data.data.results.forEach(element => {
+                const thumbnailPath = element.thumbnail.path.split('ttp')[0] + 'ttps' + element.thumbnail.path.split('ttp')[1];
+                const card = document.createElement('div');
+                card.innerHTML = `
+            <img src="${ thumbnailPath }.${ element.thumbnail.extension }">
+            <h1>${ element.name }</h1>
+        `;
+                card.className = "card";
+                charecters.appendChild(card);
+            })
+        })
+    };
